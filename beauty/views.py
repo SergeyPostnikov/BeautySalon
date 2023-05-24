91f1118885fe053c3f73e53697762c548caf4957
kruser <kruser@yandex.ru>
2023-05-24 09:38:41 +0500

    added common views

--- beauty/views.py
+++ beauty/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def admin(request):
+    return render(request, 'admin.html')
+
+def service(request):
+    return render(request, 'service.html')
+
+def service_finally(request):
+    return render(request, 'service_finally.html')
